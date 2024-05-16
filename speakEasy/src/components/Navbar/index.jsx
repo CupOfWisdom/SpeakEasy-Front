3c410c19d0fc5fc697a9f44320197d92017a9db6
Matheus Gois <mgoispaiva@gmail.com>
2024-05-16 19:59:13 -0300

    o começo

---
+++ speakEasy/src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <div className='container-nav'>
+
+        <p>LOGO</p>
+
+        <div id="btn-area">
+            <button>Experimentar</button>
+            <button>Entrar</button>
+            <button>Cadastrar</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

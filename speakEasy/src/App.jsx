fd6c250c3f3dc3f904aff97cc761b4855f98d5b1
Matheus Gois <mgoispaiva@gmail.com>
2024-05-18 22:41:25 -0300

    video e emoçoes agora aparecem

--- speakEasy/src/App.jsx
+++ speakEasy/src/App.jsx
@@ -1,12 +1,23 @@
-import Navbar from "./components/Navbar"
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
+
+
+
+import Main from "./pages/Main"
+import Input from "./pages/Input"
+import { VideoProvider } from "./utils/VideProvider"
+
+
 
 function App() {
-
-
   return (
-    <>
-      <Navbar/>
-    </>
+    <Router>
+      <VideoProvider>
+        <Routes>
+          <Route path="/" element={<Input/>}/>
+          <Route path="/video" element={<Main/>}/>
+        </Routes>
+      </VideoProvider>
+    </Router>
   )
 }
 
--- speakEasy/src/components/Navbar/index.jsx
+++ speakEasy/src/components/Navbar/index.jsx
@@ -2,14 +2,14 @@ import "./style.css";
 
 const Navbar = () => {
   return (
-    <div className='container-nav'>
+    <div className='container-nav glass'>
 
         <p>LOGO</p>
 
         <div id="btn-area">
             <button>Experimentar</button>
             <button>Entrar</button>
-            <button>Cadastrar</button>
+            <button className="btn-s">Cadastrar</button>
         </div>
     </div>
   )
